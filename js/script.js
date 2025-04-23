85fd0e06151390f8f93a69d24580747f9616e657
KoizumIsaac <208866620+isaaaaaack@users.noreply.github.com>
2025-04-23 13:41:25 -0600

    Added template...

---
+++ js/script.js
@@ -0,0 +1,88 @@
+(function ($) {
+
+  "use strict";
+
+
+  var initSearch = function () {
+    $(".user-items .search-item").click(function () {
+      $(".search-box").toggleClass('active');
+      $(".search-box .search-input").focus();
+    });
+    $(".close-button").click(function () {
+      $(".search-box").toggleClass('active');
+    });
+
+  };
+
+
+  // init Chocolat light box
+  var initChocolat = function () {
+    Chocolat(document.querySelectorAll('.image-link'), {
+      imageSize: 'contain',
+      loop: true,
+    })
+  }
+
+  var initSlider = function () {
+
+    var review_swiper = new Swiper(".review-swiper", {
+      slidesPerView: 3,
+      spaceBetween: 50,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },breakpoints: {
+        300: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+      },
+    });
+
+
+    var swiper_carousel = new Swiper(".swiper-carousel", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        300: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      },
+    });
+
+    var swiper_slideshow = new Swiper(".swiper-slideshow", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      speed: 700,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-slideshow .swiper-right',
+        prevEl: '.swiper-slideshow .swiper-left',
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+  };
+
+  // document ready
+  $(document).ready(function () {
+    initSearch();
+    initSlider();
+    initChocolat();
+
+  }); // document ready
+
+})(jQuery);
